2b9bbd88a3298d9b6d63ee8262f3194c6e32cca1
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-11-13 23:25:26 +0530

    ✨ feat: setting up route pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
+import GlobalStyles from './styles/GlobalStyles';
 import Account from './pages/Account';
 import Bookings from './pages/Bookings';
 import Cabins from './pages/Cabins';
@@ -30,19 +32,22 @@ import Users from './pages/Users';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Navigate replace to='dashboard' />} />
-        <Route path='dashboard' element={<Dashboard />} />
-        <Route path='account' element={<Account />} />
-        <Route path='bookings' element={<Bookings />} />
-        <Route path='cabins' element={<Cabins />} />
-        <Route path='login' element={<Login />} />
-        <Route path='settings' element={<Settings />} />
-        <Route path='users' element={<Users />} />
-        <Route path='*' element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to='dashboard' />} />
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='account' element={<Account />} />
+          <Route path='bookings' element={<Bookings />} />
+          <Route path='cabins' element={<Cabins />} />
+          <Route path='login' element={<Login />} />
+          <Route path='settings' element={<Settings />} />
+          <Route path='users' element={<Users />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
