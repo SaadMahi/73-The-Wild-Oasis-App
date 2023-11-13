3818da1781741010710f74b11d270851323ebf95
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-11-13 22:55:53 +0530

    ✨ feat: react router dom introduced

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,49 @@
-import styled from 'styled-components';
-import GlobalStyles from './styles/GlobalStyles';
-import Button from './ui/Button';
-import Input from './ui/Input';
-import Heading from './ui/Heading';
-import Row from './ui/Row';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Account from './pages/Account';
+import Bookings from './pages/Bookings';
+import Cabins from './pages/Cabins';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
+import Settings from './pages/Settings';
+import Users from './pages/Users';
 
-const StyledApp = styled.main`
-  /* background-color: orange; */
-  padding: 20px;
-`;
+/** REACT ROUTER
+ * 1) install react router
+ * * npm i react-router-dom
+ *
+ * 2) As we won't be using data fetching features in this application
+ * therefore we can go back setting up our routes in declarative way
+ * i) so first comes the <BrowserRouter></BrowserRouter>
+ * ii) then within this comes the <Routes></Routes> which is the one that figures our that
+ * which route matches the URL
+ * iii) then for each route we will have one route element which will be self closing
+ * elements untill they have child routes
+ *
+ * 3) next let's set the routes, also keep an default route for default route
+ * use 'index' props to set default and use 'replace' to replace the URL in the
+ * history stack and then just the route using 'to', so by using 'to' page
+ * will be directed to that URL in 'to'
+ * * <Navigatage replace to='dashboard' />
+ * ! fig: 1
+ */
 
 function App() {
   return (
-    <>
-      <GlobalStyles />
-
-      <StyledApp>
-        <Row>
-          <Row type='horizontal'>
-            <Heading as='h1'>The Wild Oasis</Heading>
-
-            <div>
-              <Heading as='h2'>Check in and out</Heading>
-              <Button onClick={() => alert('Checked in')}>Check in</Button>
-              <Button
-                variation='secondary'
-                size='small'
-                onClick={() => alert('Checked out')}
-              >
-                Check out
-              </Button>
-            </div>
-          </Row>
-
-          <Row>
-            <Heading as='h3'>Form</Heading>
-            <form>
-              <Input type='number' placeholder='Number of guests' />
-              <Input type='number' placeholder='Number of guests' />
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Navigate replace to='dashboard' />} />
+        <Route path='dashboard' element={<Dashboard />} />
+        <Route path='account' element={<Account />} />
+        <Route path='bookings' element={<Bookings />} />
+        <Route path='cabins' element={<Cabins />} />
+        <Route path='login' element={<Login />} />
+        <Route path='settings' element={<Settings />} />
+        <Route path='users' element={<Users />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
 export default App;
