328d9b2ca7cc36da76f6880a2518c301001da4dd
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-11-17 21:21:15 +0530

    ✨ feat: creating new cabins, manipulating supabase

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -7,48 +7,9 @@ import Button from '../../ui/Button';
 import FileInput from '../../ui/FileInput';
 import Textarea from '../../ui/Textarea';
 import { useForm } from 'react-hook-form';
-
-/** REACT HOOK FORM LIBRARY
- * This library can be used to greatly simplify handling forms
- * in React(SPA)
- *
- * 1) So we have our custom form
- * ! fig: 1
- * Let's set up the React hook form library
- * * npm i react-hook-form
- *
- * 2) So once installed you can notice on the form
- * we haven't made any inputs a controlled element using useState
- * ! fig: 2
- * and now we won't need to do that as we will handle everything
- * about this form using this library that we just installed
- *
- * i) so we will call the
- * * useForm hook()
- * this will return us few functions that can be used and the most fundamental one
- * is the register function and the handle submit function.
- * a) so the way this regiter works is simply in input element go into js mode
- * spread the result of calling register, and in the argument we pass in the name
- * of the field, let's just call it name which is exactly same as the id of this
- * input
- * ! fig: 3
- * b) let's observe this input filed in our component tree
- * and we find out that we get an onBlur and onChange function on it,
- * ! fig: 4
- * So all of these are placed by React hook form, simply by this marked
- * red, ok so let's place this in other input elements too.
- * c) so we are done with the step 1 to make the React form work, so always
- * the first step is to register all the input fields that we actually want
- * React hook form to handle
- *
- * 3) And then the second part is to specify the onSubmit form here
- * and here we will call that handleSubmit function we received from the useForm()
- * and pass in our own onSubmit function and this will then be called with the actual
- * data, so the data from all fileds that we registered
- * ! fig: 5
- * let's fill in the form and log the data to the console
- * ! fig: 5.1
- */
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { createCabin } from '../../services/apiCabins';
+import toast from 'react-hot-toast';
 
 const FormRow = styled.div`
   display: grid;
@@ -87,10 +48,26 @@ const Error = styled.span`
 `;
 
 function CreateCabinForm() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset } = useForm();
+
+  const queryClient = useQueryClient();
+
+  const { mutate, isLoading: isCreating } = useMutation({
+    mutationFn: createCabin,
+    onSuccess: () => {
+      toast.success('New cabin successfully created');
+      queryClient.invalidateQueries({ queryKey: ['cabin'] });
+      reset();
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  const onSubmit = (data) => {
+    mutate(data);
+  };
 
   return (
-    <Form>
+    <Form onSubmit={handleSubmit(onSubmit)}>
       <FormRow>
         <Label htmlFor='name'>Cabin name</Label>
         <Input type='text' id='name' {...register('name')} />
@@ -136,7 +113,7 @@ function CreateCabinForm() {
         <Button variation='secondary' type='reset'>
           Cancel
         </Button>
-        <Button>Submit Cabin</Button>
+        <Button disabled={isCreating}>Add Cabin</Button>
       </FormRow>
     </Form>
   );
