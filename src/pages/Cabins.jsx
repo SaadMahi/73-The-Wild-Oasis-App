e46d852f34fc69012c73e8cb9fe89c3709d507b8
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-11-17 20:13:29 +0530

    ✅ feat: added condition to display and hide form

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,8 +1,13 @@
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
 import CabinTable from '../features/cabins/CabinTable';
+import CreatCabinForm from '../features/cabins/CreateCabinForm';
+import Button from '../ui/Button';
+import { useState } from 'react';
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <>
       <Row type='horizontal'>
@@ -12,6 +17,10 @@ function Cabins() {
 
       <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm((prev) => !prev)}>
+          {!showForm ? 'Add new cabin' : 'Close Form'}
+        </Button>
+        {showForm && <CreatCabinForm />}
       </Row>
     </>
   );
